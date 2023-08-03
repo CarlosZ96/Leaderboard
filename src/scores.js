c9aa1b582c03b852ba7533f8271977c76c8a2c3d
CarlosZ96 <c4r1oszambrano@gmail.com>
2023-08-03 12:46:15 -0500

    Feature:
    Added features for getting scores and posting scores.

--- src/scores.js
+++ src/scores.js
@@ -1,15 +1,40 @@
 const BaseUrl = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api';
 
-const NewGame = async (name) => {
+const NewGame = async (Name) => {
   const response = await fetch(`${BaseUrl}/games/`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json', 
     },
     body: JSON.stringify({
-      name,
+      Name,
     }),
   });
   const data = await response.json();
   return data.result;
+};
+
+const Scores = async (GameId) => {
+  const response = await fetch(`${BaseUrl}/games/${GameId}/scores/`);
+  const data = await response.json();
+  return data.result;
+};
+
+const AddScore = async (GameId, User, Score) => {
+  await fetch(`${BaseUrl}/games/${GameId}/scores/`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      User,
+      Score,
+    }),
+  });
+};
+
+export {
+  NewGame,
+  Scores,
+  AddScore,
 };
