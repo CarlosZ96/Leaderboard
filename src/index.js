ffe91b4d0de9dfc96c9461adc0f213d3d2c0d354
CarlosZ96 <c4r1oszambrano@gmail.com>
2023-08-03 13:46:45 -0500

    Feature:
    Add index.js DOM

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import './style.css';
+import { NewGame, Scores, AddScore } from './scores.js';
+
+const SubmitButton = document.querySelector('.AddScores');
+const RefreshButton = document.querySelector('.Rbutton');
+
+let GameId = null;
+
+
+
+(async () => {
+    const GameName = 'MK';
+    GameId = await NewGame(GameName);
+  });
