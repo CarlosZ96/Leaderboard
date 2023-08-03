609da7f0f29ce93e38d711af3908fac959a2ecd5
CarlosZ96 <c4r1oszambrano@gmail.com>
2023-08-03 17:53:47 -0500

    Feacture:
    Adding event listeners for the submit and update buttons.

--- src/index.js
+++ src/index.js
@@ -4,8 +4,36 @@ import { NewGame, Scores, AddScore } from './scores.js';
 const SubmitButton = document.querySelector('.AddScores');
 const RefreshButton = document.querySelector('.Rbutton');
 
+
 let GameId = null;
 
+const Display = (Scores) => {
+  const ScoreList = document.querySelector('.ScoreList');
+  ScoreList.innerHTML = '';
+  Scores.forEach((score) => {
+    const ScoreLi = document.createElement('li');
+    ScoreLi.classList.add('ScoreI');
+    ScoreList.innerHTML = `${score.user} : ${score.score}`;
+    ScoreList.appendChild(ScoreLi);
+  });
+};
+
+RefreshButton.addEventListener('click', async () => {
+  if (GameId) {
+    const scores = await Scores(GameId);
+    displayScores(scores);
+  }
+});
+
+SubmitButton.addEventListener('submit', async (event) => {
+  event.preventDefault();
+  const NameInput = document.getElementById('NameInput');
+  const ScoreInput = document.getElementById('ScoreInput');
+  if (GameId) {
+    await AddScore(GameId, NameInput, ScoreInput);
+    SubmitButton.reset();
+  }
+});
 
 
 (async () => {
